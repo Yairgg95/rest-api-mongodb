77c95259017571899bd39271f82ccc1b4a4e781e
Yair Guadarrama <161666687+Yairgg95@users.noreply.github.com>
2025-04-22 23:51:20 -0600

    Update authControllers.js

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -4,17 +4,17 @@ const { compare } = require("../lib/encrypt");
 
 async function login(email, password) {
   const user = await User.findOne({ email: email });
-  console.log(user)
+  
   if (!user) {
-    const error = new Error("User not found");
-    error.statusCode = 404;
+    const error = new Error("Invalid credentials");
+    error.statusCode = 401;
     throw error;
   }
 
   const isPasswordValid = await compare(password, user.password);
-  console.log(isPasswordValid)
+  
   if (!isPasswordValid) {
-    const error = new Error("Invalid data");
+    const error = new Error("Invalid credentials");
     error.statusCode = 401;
     throw error;
   }
